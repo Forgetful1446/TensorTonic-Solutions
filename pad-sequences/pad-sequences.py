e3b423429eb077bcf30abdd687fcf38d7d7bd6a6
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-03-05 22:34:16 +0700

    Enhance pad_sequences with a docstring
    Add docstring to pad_sequences function.

--- pad-sequences/pad-sequences.py
+++ pad-sequences/pad-sequences.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 def pad_sequences(seqs, pad_value=0, max_len=None):
-    
+     """
+    Returns: np.ndarray of shape (N, L) where:
+      N = len(seqs)
+      L = max_len if provided else max(len(seq) for seq in seqs) or 0
+    """
     if max_len is None:
         max_len = 0
         for i in range(len(seqs)):
