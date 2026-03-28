7af2349311843f47fb92f62fdde67f248e2e81d8
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-03-28 16:55:22 +0700

    Update solution for binning

--- binning/binning.py
+++ binning/binning.py
@@ -1,17 +1,23 @@
 def binning(values, num_bins):
-	"""
-	Assign each value to an equal-width bin.
-	"""
-	max_val = max(values)
-	min_val = min(values)
-	range_bin  =  max_val - min_val
-	width = range_bin / num_bins
-	binned  = [0 for _ in range(len(values))]
-	if width == 0:
-		return binned
-	for i in range(len(values)):
-		bin_idx = int ((values[i] - min_val) / width)
-		binned[i] =  bin_idx
-		if values[i] == max_val:
-			binned[i] -= 1
-	return binned
+    max_val = max(values)
+    min_val = min(values)
+    
+    range_bin = max_val - min_val
+    width = range_bin / num_bins
+    
+    binned = [0] * len(values)
+    
+    if width == 0:
+        return binned
+    
+    for i in range(len(values)):
+        x = values[i]
+        
+        if x == max_val:
+            bin_idx = num_bins - 1
+        else:
+            bin_idx = int((x - min_val) / width)
+        
+        binned[i] = bin_idx
+    
+    return binned
