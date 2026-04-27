42a5646fa9a4806a7d93da21fb9da4411fada704
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-04-28 00:09:42 +0700

    Add solution for bernoulli-pmf

---
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def bernoulli_pmf_and_moments(x, p):
+    x = np.array(x)
+
+    pmf = np.where(x == 1, p,
+          np.where(x == 0, 1 - p, 0))
+
+    mean = p
+    var = p * (1 - p)
+
+    return pmf, mean, var
